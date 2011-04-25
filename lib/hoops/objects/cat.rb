7aebe5effa30d3ca9f78f2774a2e92b0583abc45
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2011-04-25 21:12:56 +0100

    Added a black cat variant.

--- lib/hoops/objects/cat.rb
+++ lib/hoops/objects/cat.rb
@@ -1,7 +1,7 @@
 module Hoops
   class Cat < Pet
     def initialize(command, options = {})
-      super command, "pet_cat_16x16.png", options
+      super command, "pet_cat_#{rand(2) + 1}_16x16.png", options
     end
   end
 end
